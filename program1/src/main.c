d1f80b6a54d6cbb37773df94a08f363341456afd
Xenia <xentimoshenko@gmail.com>
2023-04-18 18:33:13 +0300

    Feat:     - Some minor refactoring     - Now program2 is responsible for creating FIFO pipes if they don't exist     - Now both programs unlink these files after they've been used
    Fixed:
       - a bug when some symbols haven't been written to a FIFO file by program1 sometimes

--- program1/src/main.c
+++ program1/src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+
+void close_everything(int pipe_fd, FILE *file_fd, const char *FIFO_NAME);
 
 
 int main(int argc, char *argv[]) {
@@ -16,36 +17,36 @@ int main(int argc, char *argv[]) {
     const char *FIFO_NAME = argv[2];
 
     if (access(FIFO_NAME, F_OK) == -1) {
-        long res = mkfifo(FIFO_NAME, 0777);
-        if (res != 0) {
-            fprintf(stderr, "Couldn't create fifo%s\n", FIFO_NAME);
-            exit(EXIT_FAILURE);
-        }
+        fprintf(stderr, "Couldn't access fifo%s\n", FIFO_NAME);
+        exit(EXIT_FAILURE);
     }
 
     FILE *file_fd = fopen(FILE_NAME, "rb");
-    int pipe_fd = open(FIFO_NAME, O_WRONLY| O_NONBLOCK, 0777);
-    //char buf[1];
+    int pipe_fd = open(FIFO_NAME, O_WRONLY | O_NONBLOCK, 0777);
     int buf = 0;
     if (pipe_fd != -1 && file_fd != NULL) {
         // Пока не дошли до конца файла, выписываем все в FIFO
         while (feof(file_fd) == 0) {
-            unsigned long res = fread(&buf, sizeof buf, 1, file_fd);
-            if(res <= 0){
+            unsigned long res = fread(&buf, 1, 1, file_fd);
+            if (res <= 0) {
                 break;
             }
-            write(pipe_fd, &buf, sizeof buf);
+            write(pipe_fd, &buf, 1);
         }
-        (void)fclose(file_fd);
-        (void)close(pipe_fd);
     } else {
-        //Закрываем и если ошибка возникла только в одном из потоков
-        if(file_fd != NULL)
-            (void)fclose(file_fd);
-        if(pipe_fd != -1)
-            (void)close(pipe_fd);
+        close_everything(pipe_fd, file_fd, FIFO_NAME);
         exit(EXIT_FAILURE);
     }
+    close_everything(pipe_fd, file_fd, FIFO_NAME);
     exit(EXIT_SUCCESS);
+}
+
+void close_everything(int pipe_fd, FILE *file_fd, const char *FIFO_NAME) {
+    //Закрываем и если ошибка возникла только в одном из потоков
+    if (file_fd != NULL)
+        (void) fclose(file_fd);
+    if (pipe_fd != -1)
+        (void) close(pipe_fd);
+    unlink(FIFO_NAME);
 }
 
--- program2/src/main.c
+++ program2/src/main.c
@@ -7,15 +7,29 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 #define FIFO_NAME1 "/tmp/my_fifo1"
 #define FIFO_NAME2 "/tmp/my_fifo2"
 
-int start_program1(char* filename, char* fifo_file);
+int create_FIFO(const char *FIFO_file);
+
+int start_program1(char *filename, char *fifo_file);
+
+void close_everything(int pipe_fd1, int pipe_fd2, FILE *pipe_fd_out);
 
 int main(int argc, char *argv[]) {
+
     if (argc != 4) {
         printf("Wrong number of arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (create_FIFO(FIFO_NAME1) != 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (create_FIFO(FIFO_NAME2) != 0) {
         exit(EXIT_FAILURE);
     }
 
@@ -25,48 +39,64 @@ int main(int argc, char *argv[]) {
     FILE *pipe_fd_out = fopen(argv[3], "wb");
 
     //Запускаем и проверяем успешное выполнение программ 1
-    if(start_program1(argv[1], FIFO_NAME1) != 0
-       || start_program1(argv[2], FIFO_NAME2) != 0){
+    if (start_program1(argv[1], FIFO_NAME1) != 0
+        || start_program1(argv[2], FIFO_NAME2) != 0) {
         exit(EXIT_FAILURE);
     }
 
     long res1, res2;
-    if(pipe_fd1 != -1 && pipe_fd2 != -1 && pipe_fd_out != NULL){
+    if (pipe_fd1 != -1 && pipe_fd2 != -1 && pipe_fd_out != NULL) {
         do {
+            // Считываем по байту из каждого пайпа и выполняем XOR
             int temp1 = 0, temp2 = 0, temp = 0;
             res1 = read(pipe_fd1, &temp1, 1);
             res2 = read(pipe_fd2, &temp2, 1);
             if ((res1 > 0) && (res2 > 0)) {
-            temp = temp1 ^ temp2;
-            fwrite(&temp, 1, 1, pipe_fd_out);
+                temp = temp1 ^ temp2;
+                fwrite(&temp, 1, 1, pipe_fd_out);
             }
         } while ((res1 > 0) || (res2 > 0));
-        (void) close(pipe_fd1);
-        (void) close(pipe_fd2);
-        (void) fclose(pipe_fd_out);
-    }
-    else {
-        if(pipe_fd1 != -1)
-            (void) close(pipe_fd1);
-        if(pipe_fd2 != -1)
-            (void) close(pipe_fd2);
-        if(pipe_fd_out != NULL)
-            (void) fclose(pipe_fd_out);
+        close_everything(pipe_fd1, pipe_fd1, pipe_fd_out);
+    } else {
+        close_everything(pipe_fd1, pipe_fd1, pipe_fd_out);
         exit(EXIT_FAILURE);
     }
+
     exit(EXIT_SUCCESS);
 }
 
+/**
+ * При необходимости создает FIFO файл
+ * @param FIFO_file путь до FIFO
+ * @return 0 при удаче, -1 при ошибке
+ */
+int create_FIFO(const char *FIFO_file) {
+    if (access(FIFO_file, F_OK) == -1) {
+        long res = mkfifo(FIFO_file, 0777);
+        if (res != 0) {
+            fprintf(stderr, "Couldn't create fifo%s\n", FIFO_file);
+            return -1;
+        }
+    }
+    return 0;
+}
 
-int start_program1(char* filename, char* fifo_file){
+/**
+ * Запускает экземпляр program1 и дожидается ее выполнения
+ * @param filename путь к файлу откуда program1 считает данные
+ * @param fifo_file путь к FIFO файлу куда program1 запишет данные для IPC
+ * @return 0 при успехе, -1 при ошибке
+ */
+int start_program1(char *filename, char *fifo_file) {
+
     pid_t child_pid = fork();
-    char* args[] = {"start_program1",filename, fifo_file, NULL};
+    char *args[] = {"start_program1", filename, fifo_file, NULL};
     switch (child_pid) {
         case -1:
             perror("Error: fork failed\n");
-            break;
+            return -1;
         case 0:
-            if(execvp("./start_program1", args) == -1)
+            if (execvp("./start_program1", args) == -1)
                 perror("Error: command execution failed\n");
             exit(EXIT_FAILURE);
         default:
@@ -75,4 +105,16 @@ int start_program1(char* filename, char* fifo_file){
             }
     }
     return 0;
+}
+
+void close_everything(int pipe_fd1, int pipe_fd2, FILE *pipe_fd_out) {
+    //Закрываем и если ошибка возникла только в одном из потоков
+    if (pipe_fd1 != -1)
+        (void) close(pipe_fd1);
+    if (pipe_fd2 != -1)
+        (void) close(pipe_fd2);
+    if (pipe_fd_out != NULL)
+        (void) fclose(pipe_fd_out);
+    unlink(FIFO_NAME1);
+    unlink(FIFO_NAME2);
 }
